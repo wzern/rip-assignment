ebd9bf5b8ab33e98a3cb3f232bf2380e2479670b
Teije <tho111@uclive.ac.nz>
2025-02-19 13:04:12 +1300

    worked on sending and fectching functions

--- modules/socket_server.py
+++ modules/socket_server.py
@@ -1,14 +1,42 @@
 import socket
+import select
+import time
 
 from modules.configurator import INPUTS
-
+active_sockets = []
+routing_table = {}
+periodic_update_interval = 30
+timeout_interval = 180 
+garbage_collection_interval = 240
 def router_init():
-    active_sockets = []
-
+    
     for router_input in INPUTS:
         new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         socket_addr = ('127.0.0.1', router_input)  # Localhost and port 5000
         new_socket.bind(socket_addr)
         active_sockets.append(new_socket)
 
+
     print(active_sockets)
+
+def send():
+    for sock in active_sockets:
+        sock.sendto()
+    time.sleep(periodic_update_interval) 
+
+def fetch():
+    sockets, _, _ = select.select(active_sockets, [], [], timeout_interval)   
+    if sockets:
+        for sock in sockets:
+            update_routing_table(sock)
+    else:        
+        time.sleep(timeout_interval)
+
+
+def update_routing_table(sock):
+    data, addr = sock.recvfrom(1024) 
+    routing_update = data.decode() 
+    routing_table[addr] = routing_update
+    print(routing_table)
+
+        
--- router.py
+++ router.py
@@ -1,7 +1,8 @@
 from modules.configurator import ROUTERID, INPUTS, OUTPUTS
-from modules.socket_server import router_init
+from modules.socket_server import router_init, fetch, send
 
 router_init()
 
 while True:
-    pass
+    fetch()
+    send()
